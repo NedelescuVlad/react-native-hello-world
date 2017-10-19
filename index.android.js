8519e7a739bcbe31c9979b7d2fc69579cd51f3d7
NedelescuVlad <nedelescu.vlad@gmail.com>
2017-10-19 19:50:02 +0100

    Add webview

--- index.android.js
+++ index.android.js
@@ -16,18 +16,10 @@ import {
 export default class ReactNativeHelloWorld extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <WebView
+        source={{uri: 'https://www.viagogo.co.uk/'}}
+        style={{flex: 1}}
+      />
     );
   }
 }
