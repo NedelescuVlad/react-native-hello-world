b70297f8195f6d649e6355096d4b85be753e419c
NedelescuVlad <nedelescu.vlad@gmail.com>
2017-10-23 13:38:54 +0100

    Override hardware back button to back in the webview instead of closing

--- index.android.js
+++ index.android.js
@@ -8,12 +8,42 @@ import React, { Component } from 'react';
 import codePush from "react-native-code-push";
 import {
   AppRegistry,
-  WebView
+  WebView,
+  BackAndroid
 } from 'react-native';
 
 class ViagogoWeb extends Component {
+  state = {};
+
+  componentDidMount() {
+    BackAndroid.addEventListener('hardwareBackPress', this.backHandler);
+    this.setState({
+      backButtonEnabled: true
+    });
+  }
+
+  componentWillUnmount() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    })
+    this.setState({
+      backButtonEnabled: false
+    })
+    BackAndroid.removeEventListener('hardwareBackPress', this.backHandler);
+  }
+
+  backHandler = () => {
+    if (this.state.backButtonEnabled) {
+      this.refs["vgg_webview"].goBack();
+      return true;
+    }
+  }
+
   render() {
-    return <WebView source={{uri: 'https://www.viagogo.co.uk/'}} />
+    return <WebView 
+            ref={"vgg_webview"}
+            source={{uri: 'https://www.viagogo.co.uk/'}} />
   }
 }
 
